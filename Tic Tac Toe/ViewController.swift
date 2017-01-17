dfe5334b14f873fc6c2ad32270d1821fc75c5e94
Scott Minter <sminter@gmail.com>
2017-01-16 23:00:00 -0500

    buttons hooked up to single method

---
+++ Tic Tac Toe/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe
+//
+//  Created by Scott Minter on 1/16/17.
+//  Copyright © 2017 Scott Minter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonMove(_ sender: Any) {
+        print("Button pressed...")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
